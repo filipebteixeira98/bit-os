da2cfcae61b2fd421cb5a1a807f353a1b3eb1c33
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-05-12 14:00:25 -0300

    feat(boot): expand kernel loading to handle larger binaries

--- boot.asm
+++ boot.asm
@@ -13,3 +13,18 @@ call switch_to_pm
 jmp $
 
 BOOT_DRIVE db 0
+
+load_kernel:
+  mov bx, KERNEL_OFFSET
+  mov dh, 15
+
+  mov ah, 0x02
+  mov al, dh
+  mov ch, 0x00
+  mov cl, 0x02
+  mov dh, 0x00
+  mov dl, [BOOT_DRIVE]
+
+  int 0x13
+
+  ret
