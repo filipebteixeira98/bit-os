451cdc87edab0a5ae1d6a16d00c287dc1621bbf1
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-05-12 14:09:52 -0300

    feat(gdt): implement core GDT structures for x86 transition

--- boot.asm
+++ boot.asm
@@ -28,3 +28,31 @@ load_kernel:
   int 0x13
 
   ret
+
+gdt_start:
+
+gdt_null:
+  dd 0x0
+  dd 0x0
+
+gdt_code:
+  dw 0xffff
+  dw 0x0000
+  db 0x00
+  db 10011010b
+  db 11001111b
+  db 0x00
+
+gdt_data:
+  dw 0xffff
+  dw 0x0000
+  db 0x00
+  db 10010010b
+  db 11001111b
+  db 0x00
+
+gdt_end:
+
+gdt_descriptor:
+  dw gdt_end - gdt_start - 1
+  dd gdt_start
