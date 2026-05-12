5809803cce3bbf43c59b97670ca5a5f3ecba0c82
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-05-12 13:17:19 -0300

    chore(boot): improve kernel loading and error handling

--- boot.asm
+++ boot.asm
@@ -1,16 +1,35 @@
 [org 0x7c00]
 
-mov ah, 0x0e
-mov al, 'O'
-int 0x10
+mov ax, 0x0000
+mov es, ax
 
-mov al, 'S'
-int 0x10
+mov bx, 0x1000
+mov dh, 2
 
-mov al, '!'
-int 0x10
+call load_kernel
 
-jmp $
+jmp 0x0000:0x1000
+
+load_kernel:
+  mov ah, 0x02
+  mov al, dh
+  mov ch, 0x00
+  mov cl, 0x02
+  mov dh, 0x00
+  mov dl, 0x80
+
+  int 0x13
+
+  jc disk_error
+  
+  ret
+
+disk_error:
+  mov ah, 0x0e
+  mov al, 'E'
+  int 0x10
+
+  jmp $
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
