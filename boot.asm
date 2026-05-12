301a1135568353ec4d4c82ca8060683d83921142
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-05-12 13:52:44 -0300

    refactor(boot): implement GDT, stack setup, and BIOS drive persistence

--- boot.asm
+++ boot.asm
@@ -1,35 +1,15 @@
 [org 0x7c00]
 
-mov ax, 0x0000
-mov es, ax
+KERNEL_OFFSET equ 0x1000
 
-mov bx, 0x1000
-mov dh, 1
+mov [BOOT_DRIVE], dl
+
+mov bp, 0x9000
+mov sp, bp
 
 call load_kernel
+call switch_to_pm
 
-jmp 0x0000:0x1000
+jmp $
 
-load_kernel:
-  mov ah, 0x02
-  mov al, dh
-  mov ch, 0x00
-  mov cl, 0x02
-  mov dh, 0x00
-  mov dl, 0x80
-
-  int 0x13
-
-  jc disk_error
-  
-  ret
-
-disk_error:
-  mov ah, 0x0e
-  mov al, 'E'
-  int 0x10
-
-  jmp $
-
-times 510 - ($ - $$) db 0
-dw 0xaa55
+BOOT_DRIVE db 0
