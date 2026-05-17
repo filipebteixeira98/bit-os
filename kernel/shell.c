415bb0a42cd3bf1c9ffd9c46859178feaba00c5d
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-05-17 19:31:11 -0300

    feat(shell): add custom strcmp utility function

--- kernel/shell.c
+++ kernel/shell.c
@@ -10,3 +10,16 @@ int command_index = 0;
 void shell_init() {
   print("> ");
 }
+
+int strcmp(const char *a, const char *b) {
+  int i = 0;
+
+  while (a[i] && b[i]) {
+    if (a[i] != b[i]) 
+      return 0;
+  
+    i++;
+  }
+
+  return a[i] == b[i];
+}
